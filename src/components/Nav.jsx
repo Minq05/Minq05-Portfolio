a573ea1ba1a020e17c7a55057979ff5f88446fc8
Minq05 <hoangquan22022005@gmail.com>
2025-04-04 19:22:06 +0700

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,12 +5,6 @@ const Nav = () => {
     <nav className="w-full text-white p-4 flex justify-between items-center">
       <div className="flex items-center">
         <div className="rounded-full overflow-hidden">
-          <Link to="/">
-            <img
-              src="https://s3-alpha-sig.figma.com/img/6c97/ed47/76b8c84d7f330ed6b4d524c5013d50f1?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=X52fezzW6ji~1DUtYxwV0rAUxakZxz~iYtSSWRSVqpCAJbvu--7Tol7ioWQfGg6R4uLbe5IqctXxlh4QranRu6OeNXxsCiEex8Ms5sMx0XV-v-~fUZOQmyA72ZML4IVaiMV56vMX08Pg5z2W5TysCsL9YlNVPgUi04xR1my2HTmrKedahqakoO0AyEv59BetfKm3TE95cXv3237u70DZP-L~oiV8tZmS5aG6olY8v88O5YXkZzaGD5mqCxAvqBaYrl9~vtJctR04mpA9JZh4kaNROp9EMv~BnNpOplKMlPohKuLMmZdaz6k6O4lR-7N6F9mVrBlwoq3M0SGpOpmBVw__"
-              className="w-20 h-20 object-cover"
-            />
-          </Link>
         </div>
       </div>
       <ul className="flex space-x-6 mr-6">
